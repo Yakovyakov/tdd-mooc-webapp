8cc12417b68e0102793435244f93aa477433242f
Yakovyakov <yyak0423@gmail.com>
2025-03-28 10:34:06 -0400

    t Change e2e for check in backend endpoint health

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 frontend_response=$(curl --silent --show-error http://localhost:8080/hello.html)
 test "$frontend_response" = "Hello from frontend"
 
-backend_response=$(curl --silent --show-error http://localhost:8080/api/hello.html)
-test "$backend_response" = "Hello from backend"
+backend_response=$(curl --silent --show-error http://localhost:8080/api/health)
+test "echo \"$backend_response\" | grep '\"status\":\"OK\""
 
 : OK
