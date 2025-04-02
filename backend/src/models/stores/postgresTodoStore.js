3a7d9432315076feb4b00a8c20e47a549ff79cde
Yakovyakov <yyak0423@gmail.com>
2025-04-02 10:23:30 -0400

    r postgresTodoStore

--- backend/src/models/stores/postgresTodoStore.js
+++ backend/src/models/stores/postgresTodoStore.js
@@ -2,22 +2,24 @@ class PostgresTodoStore {
   constructor(db) {
       this.db = db;
   }
-
+  #rowToTodo(row) {
+    return { id: row.id, title: row.title, completed: row.completed };
+  }
   async getAll() {
     const result = await this.db.query(`
       SELECT id, title, completed
       FROM todos
       WHERE archived = false
-    `); // ¡No selecciona el campo 'archived'!
+    `);
     return result.rows;
   }
 
   async create({ title }) {
-    const { rows } = await this.db.query(
+    const { rows }  = await this.db.query(
       'INSERT INTO todos (title) VALUES ($1) RETURNING id, title, completed',
       [title]
     );
-    return rows;
+    return rows.map(this.#rowToTodo)[0] || null;
   }
 }
 
