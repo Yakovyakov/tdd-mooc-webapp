9e8161e07010457440b999629d98f1b57792b64e
Yakovyakov <yyak0423@gmail.com>
2025-04-02 16:33:31 -0400

    r app and todoRoutes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 
-const { checkConnection } = require('./config/db');
+const bodyParser = require('body-parser');
+
+const { pool, checkConnection } = require('./config/db');
+
+const TodoService = require('./services/todoService');
+const PostgresTodoStore = require('./models/stores/postgresTodoStore');
+const TodoController = require('./controllers/todoController');
+const Todo = require('./models/Todo');
+
+const todoRoutes = require('./routes/todoRoutes');
+
 
 const app = express();
+
+const postgressTodoStore = new PostgresTodoStore(pool);
+const todoModel = new Todo(postgressTodoStore);
+const todoService = new TodoService(todoModel);
+const todoController = new TodoController(todoService);
+// Configurar rutas
+
+app.use(bodyParser.json());
+app.use('/todos', todoRoutes(todoController));
 
 app.get('/api/health', async (req, res) => {
   const dbStatus = await checkConnection();
---
+++ backend/src/routes/todoRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (controller) => {
+  router.get('/', controller.getTodos.bind(controller));
+  router.post('/', controller.addTodo.bind(controller));
+  
+  return router;
+};
