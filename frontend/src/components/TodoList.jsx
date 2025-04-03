188ed4fba8cb02534c8bdf63747da966db8992b2
Yakovyakov <yyak0423@gmail.com>
2025-04-03 12:45:13 -0400

    r TodoList component: added key to list elements

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -6,9 +6,16 @@ const TodoList = ({ todos }) => {
   return (
     <>
       {
-        todos.map(
-          todo => <Todo todo={todo} />
-        ).reduce((acc, cur) => [...acc, <hr />, cur], [])
+        todos.map((todo, index) => (
+            <React.Fragment key={todo.id}>
+              <Todo todo={todo} />
+              {
+                index < todos.length - 1 && (
+                  <hr key={`separator-${todo.id}`} />
+                )
+              }
+            </React.Fragment>
+        ))
       }
     </>
   )
