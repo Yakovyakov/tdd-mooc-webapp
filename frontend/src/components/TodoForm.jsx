76912fb55765220e84e39fe897694c3adfa56c52
Yakovyakov <yyak0423@gmail.com>
2025-04-03 11:57:19 -0400

    r implement TodoForm component

---
+++ frontend/src/components/TodoForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const TodoForm = ({ createTodo }) => {
+  const [title, setTitle] = useState('')
+
+  const onChange = ({ target }) => {
+    setTitle(target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createTodo({ title })
+    setTitle('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input className="titleInput" type="text" name="title" value={title} onChange={onChange} />
+      <button type="submit"> Create </button>
+    </form>
+  )
+}
+
+export default TodoForm
