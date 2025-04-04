39eceb272e373581071214b112136a1ef7b45952
Yakovyakov <yyak0423@gmail.com>
2025-04-04 12:21:18 -0400

    r todoService: implemented updateTodo

--- backend/src/services/todoService.js
+++ backend/src/services/todoService.js
@@ -18,6 +18,15 @@ class TodoService {
     });
   }
 
+  async updateTodo(id, updates) {
+    const todo = await this.model.getById(id);
+
+
+    if (!todo) throw new Error('Todo not found');
+
+    return this.model.update(id, updates);
+  }
+
 }
 
 
