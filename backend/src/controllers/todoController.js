6ae723742f2e329c47d3cb002a9b12b1b111b7cb
Yakovyakov <yyak0423@gmail.com>
2025-04-07 19:09:21 -0400

    r todoController: added genericUpdate

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -26,6 +26,40 @@ class TodoController {
       res.status(500).json({ error: error.message });
     }
   }
+
+  async genericUpdate(req, res) {
+    try {
+      // only allowedFields
+      const { id } = req.params;
+      const updates = req.body;
+      const allowedFields = ['completed'];
+      const filteredUpdates = Object.keys(updates)
+        .filter(key => allowedFields.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = updates[key];
+          return obj;
+        }, {});
+
+      if (Object.keys(filteredUpdates).length === 0) {
+        return res.status(400).json({ error: 'No valid fields to update' });
+      }
+
+      const { completed } = filteredUpdates;
+
+      if (completed && typeof completed !== 'boolean') {
+        return res.status(400).json({ error: 'No valid fields to update' });
+      }
+
+      const updatedTodo = await this.service.updateTodo(id, { filteredUpdates });
+      res.status(200).json(updatedTodo);
+    } catch (error) {
+      if (error.message === 'Todo not found') {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+      res.status(500).json({ error: error.message });
+    }
+  }
+
 }
   
 module.exports = TodoController;
