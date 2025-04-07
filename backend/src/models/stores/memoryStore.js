f3d1772c934ff5528620f1ac7a90fcaa1e2bdd4d
Yakovyakov <yyak0423@gmail.com>
2025-04-07 13:11:32 -0400

    r memoryStore: added function update

--- backend/src/models/stores/memoryStore.js
+++ backend/src/models/stores/memoryStore.js
@@ -12,6 +12,7 @@ class MemoryStore {
 
     return todoWithoutArchived;
   }
+
   async getAll() {
     return this.todos
       .filter(todo => !todo.archived)
@@ -22,6 +23,35 @@ class MemoryStore {
         // not includes 'archived' = true!
       }));
   }
+
+  async update(id, updates) {
+
+    const index = this.todos.findIndex(todo => todo.id === id);
+    if (index === -1) return null;
+
+    // only allowedFields
+    const allowedFields = ['completed'];
+    const filteredUpdates = Object.keys(updates)
+      .filter(key => allowedFields.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = updates[key];
+        return obj;
+      }, {});
+
+    if (Object.keys(filteredUpdates).length === 0) {
+      throw new Error('No valid fields to update');
+    }
+
+    this.todos[index] = { 
+      ...this.todos[index], 
+      ...filteredUpdates 
+    };
+
+    const todo = this.todos[index];
+    const { archived, ...todoWithoutArchived } = todo;
+    return todoWithoutArchived;
+  }
+
 
   async create(todo) {
     const newTodo = {
