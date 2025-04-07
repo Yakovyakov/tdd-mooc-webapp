683631d5d85cfe5f1534eef23777b613f201d296
Yakovyakov <yyak0423@gmail.com>
2025-04-07 19:58:09 -0400

    r todoRoutes: added route for patch

--- backend/src/routes/todoRoutes.js
+++ backend/src/routes/todoRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 module.exports = (controller) => {
   router.get('/', controller.getTodos.bind(controller));
   router.post('/', controller.addTodo.bind(controller));
+  router.patch('/:id', controller.genericUpdate.bind(controller));
   
   return router;
 };
