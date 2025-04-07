b69fdca7829040e7a8c4bcdafcafb81f6dd2b159
Yakovyakov <yyak0423@gmail.com>
2025-04-07 13:48:40 -0400

    r Todo Model: added function update

--- backend/src/models/Todo.js
+++ backend/src/models/Todo.js
@@ -1,25 +1,28 @@
 class Todo {
-    constructor(store) {
-      this.store = store;
-    }
-  
-    async create(todo) {
-      if (!todo.title?.trim()) {
-        throw new Error('Title is required');
-      }
-      return this.store.create({
-        title: todo.title.trim(),
-      });
-    }
-
-    async getById(id) {
-      return this.store.getById(id);
-    }
-  
-  
-    async getAll() {
-      return this.store.getAll();
-    }
+  constructor(store) {
+    this.store = store;
   }
+
+  async create(todo) {
+    if (!todo.title?.trim()) {
+      throw new Error('Title is required');
+    }
+    return this.store.create({
+      title: todo.title.trim(),
+    });
+  }
+
+  async getById(id) {
+    return this.store.getById(id);
+  }
+
+  async update(id, updates) {
+    return this.store.update(id, updates);
+  }
+  
+  async getAll() {
+    return this.store.getAll();
+  }
+}
   
   module.exports = Todo;
