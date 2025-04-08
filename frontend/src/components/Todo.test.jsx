62ee8e28deaf69c4e38c4bd1f22d5389ab9923b4
Yakovyakov <yyak0423@gmail.com>
2025-04-07 21:07:10 -0400

    t correct test for Todo component

--- frontend/src/components/Todo.test.jsx
+++ frontend/src/components/Todo.test.jsx
@@ -25,9 +25,17 @@ describe('Todo component tests', () => {
       element = screen.getByText('This todo is not done')
       expect(element).toBeDefined()
       expect(element).toBeVisible()
+      const button = screen.getByRole('button', { name: /set as done/i })
+      expect(button).toBeDefined()
+      expect(button).toBeVisible();
+
+      const buttonArchive = screen.getByRole('button', { name: /archive/i })
+      expect(buttonArchive).toBeDefined()
+      expect(buttonArchive).toBeVisible();
+
 
     })
-    test('if completed is true, button archived is visible', () => {
+    test('if completed is true, button set as done is not visible', () => {
       const todo = {
         id: '1234',
         title: 'title value',
@@ -42,9 +50,13 @@ describe('Todo component tests', () => {
       element = screen.getByText('This todo is done')
       expect(element).toBeDefined()
       expect(element).toBeVisible()
-      element = screen.getByText('Archive')
-      expect(element).toBeDefined()
-      expect(element).toBeVisible()
+
+      const button = screen.queryByRole('button', { name: /set as done/i })
+      expect(button).toBeNull()
+
+      const buttonArchive = screen.getByRole('button', { name: /archive/i })
+      expect(buttonArchive).toBeDefined()
+      expect(buttonArchive).toBeVisible();
 
     })
   })
