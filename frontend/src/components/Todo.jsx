71122ad752c9ac2d5da0d6e7ee7bba2ab27fce33
Yakovyakov <yyak0423@gmail.com>
2025-04-08 01:15:51 -0400

    r component Todo: rename a todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
+const Todo = ({ todo, completeTodo, renameTodo }) => {
 
-const Todo = ({ todo, completeTodo }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title);
+
   const onClickComplete = () => {
     completeTodo(todo.id, { completed: true })
   }
+
+  const onClickSave = () => {
+    renameTodo(todo.id, { title: editedTitle })
+  }
+
   const doneInfo = (
     <>
       <span>This todo is done</span>
@@ -25,9 +34,38 @@ const Todo = ({ todo, completeTodo }) => {
   )
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-      <span>
-        {todo.title}
-      </span>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedTitle}
+          onChange={(e) => setEditedTitle(e.target.value)}
+          autoFocus
+        /> 
+      ) : (
+        <span>
+          {todo.title}
+        </span>
+      )}
+      <div>
+        
+        {!isEditing && (
+          <button onClick={() => setIsEditing(true)}>
+            Rename
+          </button>
+        )}
+        
+        {isEditing && (
+          <>
+            <button onClick={onClickSave}>
+              Save
+            </button>
+            <button onClick={() => setIsEditing(false)}>
+              Cancel
+            </button>
+          </>
+        )}
+        
+      </div>
       {todo.completed ? doneInfo : notDoneInfo}
     </div>
   )
